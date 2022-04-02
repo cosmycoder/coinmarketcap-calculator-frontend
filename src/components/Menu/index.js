965868365fdacfd849410b41e34342bc9f8fffff
cosmycoder <cosmycoder@gmail.com>
2022-04-02 08:22:59 +0300

    update menu for mobile

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,13 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { Layout } from "antd";
+import { Layout, Menu, Dropdown } from "antd";
+import { MenuOutlined } from '@ant-design/icons';
 import FooterPage from "components/Footer";
-import MenuItems from "./MenuItems";
 import "./menu.less";
 
 const { Header, Content, Sider, Footer } = Layout;
+const { SubMenu } = Menu;
 
 const MainLayout = ({ children }) => {
+  const [current, setCurrent] = useState("cryptocurrency");
+
+  const MenuItems = (
+    <>
+      <Menu.Item className="submenu-item" key="pricing">
+        <NavLink to="/pricing">
+          <div className="submenu-text">
+            Pricing
+          </div>
+        </NavLink>
+      </Menu.Item>
+      <SubMenu key="SubMenu" title="Cryptocurrency Calculators">
+        <Menu.Item className="submenu-item" key="cryptocurrency">
+          <NavLink to="/cryptocurrencyconversioncalculator">
+            <div className="submenu-text">
+              Cryptocurrency Conversion Calculator
+            </div>
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item className="submenu-item" key="profitlosscalculator">
+          <NavLink to="/profitlosscalculator">
+            <div className="submenu-text">
+              Profit Loss Calculator
+            </div>
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item className="submenu-item" key="cryptoprofitcalculator">
+          <NavLink to="/cryptoprofitcalculator">
+            <div className="submenu-text">
+              Crypto Profit Calculator
+            </div>
+          </NavLink>
+        </Menu.Item>
+      </SubMenu>
+    </>
+  )
+
+  const DesktopMenu = (
+    <Menu
+      onClick={(e) => setCurrent(e.key)}
+      selectedKeys={[current]}
+      mode="horizontal"
+    >
+      { MenuItems }
+    </Menu>
+  )
+
+  const MobileMenu = (
+    <Menu
+      onClick={(e) => setCurrent(e.key)}
+      selectedKeys={[current]}
+    >
+      { MenuItems }
+    </Menu>
+  )
+
   return (
     <Layout className="menu-layout">
       <Header>
@@ -20,14 +77,14 @@ const MainLayout = ({ children }) => {
           />
         </NavLink>
         <div className="mobile-menu">
-          {/* <Dropdown overlay={menu}>
+          <Dropdown overlay={MobileMenu}>
             <div className="ant-dropdown-link cursor-pointer" onClick={e => e.preventDefault()}>
               <MenuOutlined />
             </div>
-          </Dropdown> */}
+          </Dropdown>
         </div>
         <div className="desktop-menu">
-          <MenuItems />
+          { DesktopMenu }
         </div>
       </Header>
       <Layout>
